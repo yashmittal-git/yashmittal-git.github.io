dd7bf01cde9a23d5b3a25936c6271d15b3fcdd8d
Yash Mittal <mittal.yash2000@gmail.com>
2023-05-27 18:18:17 +0530

    Typed, subpath for git

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,33 @@
 // Scripts
 // 
 
+(function() {
+    "use strict";
+
+    const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+          return [...document.querySelectorAll(el)]
+        } else {
+          return document.querySelector(el)
+        }
+      }    
+
+    const typed = select('.typed')
+    if (typed) {
+    let typed_strings = typed.getAttribute('data-typed-items')
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+        strings: typed_strings,
+        // loop: true,
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 2000
+    });
+    }
+
+})()
+
 window.addEventListener('DOMContentLoaded', event => {
 
     // Activate Bootstrap scrollspy on the main nav element
@@ -32,3 +59,8 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+if (window.location.href.endsWith("/git")) {
+    // Redirect to the desired link
+    window.location.href = "https://github.com/yashmittal-git/";
+}
