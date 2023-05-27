da29763f81d41fa34f8c38f61890617cda0af5e4
Yash Mittal <mittal.yash2000@gmail.com>
2023-05-28 02:23:25 +0530

    fixed intro, experience and education

--- js/scripts.js
+++ js/scripts.js
@@ -25,10 +25,10 @@
     typed_strings = typed_strings.split(',')
     new Typed('.typed', {
         strings: typed_strings,
-        // loop: true,
-        typeSpeed: 100,
-        backSpeed: 50,
-        backDelay: 2000
+        loop: true,
+        typeSpeed: 150,
+        backSpeed: 40,
+        backDelay: 1500
     });
     }
 
@@ -64,3 +64,41 @@ if (window.location.href.endsWith("/git")) {
     // Redirect to the desired link
     window.location.href = "https://github.com/yashmittal-git/";
 }
+
+function populateListFromJson(jsonFile, listId) {
+    fetch(jsonFile)
+      .then(response => response.json())
+      .then(data => {
+        const list = document.getElementById(listId);
+        data.forEach(item => {
+          const listItem = createListItem(item);
+          list.appendChild(listItem);
+        });
+      })
+      .catch(error => {
+        console.error('Error fetching JSON:', error);
+      });
+  }
+
+  function createListItem(item) {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `
+      <span class="fa-li"><i class="fas fa-chevron-right"></i></span>
+      ${item.text}
+    `;
+
+    if (item.children) {
+      const nestedList = document.createElement('ul');
+      nestedList.classList.add('fa-ul', 'mb-0');
+      item.children.forEach(child => {
+        const nestedListItem = createListItem(child);
+        nestedList.appendChild(nestedListItem);
+      });
+      listItem.appendChild(nestedList);
+    }
+
+    return listItem;
+  }
+
+  populateListFromJson('assets/data/amadeus-sde-1.json', 'amadeus-sde-1-experience');
+  populateListFromJson('assets/data/amadeus-sde-intern.json', 'amadeus-sde-intern-experience');
