2e48b2d59d307b624507e1ad909498621dc1d7bb
Yash Mittal <yash.mittal@convin.ai>
2025-12-06 23:57:14 +0530

    Added Voicebot Experience in portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -7,102 +7,103 @@
 // Scripts
 // 
 
-(function() {
-    "use strict";
+(function () {
+  "use strict";
 
-    const select = (el, all = false) => {
-        el = el.trim()
-        if (all) {
-          return [...document.querySelectorAll(el)]
-        } else {
-          return document.querySelector(el)
-        }
-      }    
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
 
-    const typed = select('.typed')
-    if (typed) {
+  const typed = select('.typed')
+  if (typed) {
     let typed_strings = typed.getAttribute('data-typed-items')
     typed_strings = typed_strings.split(',')
     new Typed('.typed', {
-        strings: typed_strings,
-        loop: true,
-        typeSpeed: 100,
-        backSpeed: 40,
-        backDelay: 800
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 40,
+      backDelay: 800
     });
-    }
+  }
 
 })()
 
 window.addEventListener('DOMContentLoaded', event => {
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.body.querySelector('#sideNav');
-    if (sideNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#sideNav',
-            rootMargin: '0px 0px -40%',
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
+  // Activate Bootstrap scrollspy on the main nav element
+  const sideNav = document.body.querySelector('#sideNav');
+  if (sideNav) {
+    new bootstrap.ScrollSpy(document.body, {
+      target: '#sideNav',
+      rootMargin: '0px 0px -40%',
     });
+  };
+
+  // Collapse responsive navbar when toggler is visible
+  const navbarToggler = document.body.querySelector('.navbar-toggler');
+  const responsiveNavItems = [].slice.call(
+    document.querySelectorAll('#navbarResponsive .nav-link')
+  );
+  responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItem.addEventListener('click', () => {
+      if (window.getComputedStyle(navbarToggler).display !== 'none') {
+        navbarToggler.click();
+      }
+    });
+  });
 
 });
 
 if (window.location.href.endsWith("/git")) {
-    // Redirect to the desired link
-    window.location.href = "https://github.com/yashmittal-git/";
+  // Redirect to the desired link
+  window.location.href = "https://github.com/yashmittal-git/";
 }
 
 function populateListFromJson(jsonFile, listId) {
-    fetch(jsonFile)
-      .then(response => response.json())
-      .then(data => {
-        const list = document.getElementById(listId);
-        data.forEach(item => {
-          const listItem = createListItem(item);
-          list.appendChild(listItem);
-        });
-      })
-      .catch(error => {
-        console.error('Error fetching JSON:', error);
+  fetch(jsonFile)
+    .then(response => response.json())
+    .then(data => {
+      const list = document.getElementById(listId);
+      data.forEach(item => {
+        const listItem = createListItem(item);
+        list.appendChild(listItem);
       });
-  }
+    })
+    .catch(error => {
+      console.error('Error fetching JSON:', error);
+    });
+}
 
-  function createListItem(item) {
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `
+function createListItem(item) {
+  const listItem = document.createElement('li');
+  listItem.innerHTML = `
       <span class="fa-li"><i class="fas fa-chevron-right"></i></span>
       ${item.text}
     `;
 
-    if (item.children) {
-      const nestedList = document.createElement('ul');
-      nestedList.classList.add('fa-ul', 'mb-0');
-      item.children.forEach(child => {
-        const nestedListItem = createListItem(child);
-        nestedList.appendChild(nestedListItem);
-      });
-      listItem.appendChild(nestedList);
-    }
-
-    return listItem;
+  if (item.children) {
+    const nestedList = document.createElement('ul');
+    nestedList.classList.add('fa-ul', 'mb-0');
+    item.children.forEach(child => {
+      const nestedListItem = createListItem(child);
+      nestedList.appendChild(nestedListItem);
+    });
+    listItem.appendChild(nestedList);
   }
 
-  populateListFromJson('assets/data/convin-tech-lead.json', 'convin-tech-lead-experience');
-  populateListFromJson('assets/data/amadeus-sde-1.json', 'amadeus-sde-1-experience');
-  populateListFromJson('assets/data/amadeus-sde-intern.json', 'amadeus-sde-intern-experience');
-  populateListFromJson('assets/data/summer-intern-eced.json', 'summer-intern-eced-experience');
-  populateListFromJson('assets/data/portfolio-website.json', 'portfolio-website-project');
-  populateListFromJson('assets/data/finance-tracker.json', 'finance-tracker-project');
+  return listItem;
+}
+
+populateListFromJson('assets/data/convin-tech-lead-voicebot.json', 'convin-tech-lead-voicebot-experience');
+populateListFromJson('assets/data/convin-tech-lead.json', 'convin-tech-lead-experience');
+populateListFromJson('assets/data/amadeus-sde-1.json', 'amadeus-sde-1-experience');
+populateListFromJson('assets/data/amadeus-sde-intern.json', 'amadeus-sde-intern-experience');
+populateListFromJson('assets/data/summer-intern-eced.json', 'summer-intern-eced-experience');
+populateListFromJson('assets/data/portfolio-website.json', 'portfolio-website-project');
+populateListFromJson('assets/data/finance-tracker.json', 'finance-tracker-project');
